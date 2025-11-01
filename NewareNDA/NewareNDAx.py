7e0601a412c569a5054b0c4b677126d54015cc1c
Dan Cogswell <cogswell@alum.mit.edu>
2025-11-01 11:08:56 -0400

    Corrects the Step field.

--- NewareNDA/NewareNDAx.py
+++ NewareNDA/NewareNDAx.py
@@ -605,6 +605,7 @@ def _read_ndc_16_filetype_18(mm):
         'Charge_Capacity(mAh)', 'Discharge_Capacity(mAh)',
         'Charge_Energy(mWh)', 'Discharge_Energy(mWh)',
         'Timestamp', 'Step', 'Index'])
+    df["Step"] = _count_changes(df["Step"])
 
     # Convert timestamp to local timezone
     tz = datetime.now().astimezone().tzinfo
